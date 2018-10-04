82d1241c02337469a4bb0d7ce60f335f9cc49388
Jim Witschey <jim.witschey@gmail.com>
2018-10-04 09:38:59 -0400

    fixup

--- cassandra/io/libevreactor.py
+++ cassandra/io/libevreactor.py
@@ -343,12 +343,11 @@ class LibevConnection(Connection):
                 if len(buf) < self.in_buffer_size:
                     break
         except socket.error as err:
-            if ssl and isinstance(err, ssl.SSLError):
-                if err.args[0] not in (ssl.SSL_ERROR_WANT_READ, ssl.SSL_ERROR_WANT_WRITE):
-                    self.defunct(err)
-                    return
-            elif err.args[0] not in NONBLOCKING:
+            log.debug('got error {}'.format(err))
+            if not is_expected_nonblocking_socket_error(err):
                 self.defunct(err)
+                log.debug('... but not defuncting')
+            else:
                 return
 
         if self._iobuf.tell():
